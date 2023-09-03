93fc3d6b0c4e481886b5a20fcd965a64b7d6113d
Coral <coralelkanar@gmail.com>
2023-09-03 14:24:40 +0300

    added new file (chapter3 - resize and crop functions) and new image

---
+++ chap3_resize_crop.py
@@ -0,0 +1,28 @@
+import cv2
+
+# the convention of x and y axes in opencv:
+
+"""
+(0,0) -----------> X 
+      |
+      |
+      |
+      v
+      Y
+ """
+
+# ******************** Converting the image to grayscale ********************
+
+# in order to resize an image we need to know the current size of the image
+
+# import an image
+img = cv2.imread("Resources/snow_in_Bolivia.png.png")
+
+# print the size of the image: (height, width, number of channels - BGR=3)
+print(img.shape)
+
+# arguments of the functions resize - 1) the image, 2) (height, width)
+# imgResize = cv2.resize(img, ())
+
+cv2.imshow("image", img)
+cv2.waitKey(0)
